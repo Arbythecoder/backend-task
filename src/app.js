2298f9f3c4e695a884a18d5c19df01ab6db2a6aa
Arbythecoder <houseofarby@gmail.com>
2023-09-09 10:43:18 +0100

    all files added

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/api", (req, res) => {
+  const slackName = req.query.slack_name || "Afolabi Abigeal";
+  const track = req.query.track || "backend";
+  const currentDate = new Date();
+  const currentDay = currentDate.toLocaleDateString("en-NG", {
+    weekday: "long",
+  });
+  const utcTime = currentDate.toISOString();
+
+  // Construct dynamic GitHub URLs based on the file and repository
+  const githubUsername = "Arbythecoder"; // GitHub username
+  const githubRepo = "backend-task"; // repository name
+  const githubFilePath = "HNGX\backend-task.ext"; //
+
+  const githubFileUrl = `https://github.com/Arbythecoder/backend-task/blob/main/\HNGX\backend-task.ext`;
+  const githubRepoUrl = `https://github.com/Arbythecoder/backend-task`;
+
+  const status_code = 200;
+
+  // Construct the JSON response
+  const jsonResponse = {
+    slack_name: slackName,
+    current_day: currentDay,
+    utc_time: utcTime,
+    track: track,
+    github_file_url: githubFileUrl,
+    github_repo_url: githubRepoUrl,
+    status_code: status_code,
+  };
+
+  res.json(jsonResponse);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
